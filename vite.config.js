4a9b5d51ef17a9274b4245bd1c4c9152cc28af3b
TyrelM10 <tjmenezes08@gmail.com>
2023-07-04 14:56:51 +0100

    Added changes before vite deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/tyrel-menezes/',
+  base: '/tyrelmenezes_portfolio/',
   plugins: [react()],
 })
