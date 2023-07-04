0169960155d55d8056795e0174c83c42c712522a
TyrelM10 <tjmenezes08@gmail.com>
2023-07-04 17:23:05 +0100

    Added Preloader Modules and Styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import { BrowserRouter } from "react-router-dom";
+import { useState } from "react";
 import { About, Contact , Experience, Feedbacks, Hero, Navbar, StarsCanvas, Tech, Works } from './components';
 const App = () => {
+  const [loading, setLoading] = useState(true);
+  const spinner = document.getElementById('preloader');
+
+  if(spinner){
+    setTimeout(()=>{
+      spinner.style.display = "none";
+      setLoading(false);}, 2000);
+    }
   return (
+    !loading && (
     <BrowserRouter>
     <div className="relative z-0 bg-primary">
     <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
@@ -19,7 +29,7 @@ const App = () => {
     </div>
     </div>
     </BrowserRouter>
-  )
+    ))
 }
 
 export default App
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import preload from "vite-plugin-preload";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/tyrelmenezes_portfolio/',
-  plugins: [react()],
+  plugins: [react(), preload()],
 })
